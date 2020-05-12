7c04bae3235303c3d93699650ffb399012f352fc
moses4real <61254175+moses4real@users.noreply.github.com>
2020-05-12 02:09:17 +0100

    Add files via upload

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request, render_template
 from app import app
 
 @app.route('/')
@@ -11,7 +11,34 @@ def hello():
 def about():
     return 'We are the knights who say Ni!!';
 
-@app.route('/question/<title>')
+@app.route('/question/<title>', methods=['GET', 'POST'])
 def question(title):
-    message = 'you said ' + title;
-    return message;
+    if request.method == 'GET':
+        # Redis code to load question
+        return render_template('AnswerQuestion.html',
+                               question = question)
+    elif request.method == 'POST':
+        submittedAnswer = request.form['submittedAnswer'];
+        # Redis code to load answer
+        if submittedAnswer == answer:
+            return render_template('Correct.html');
+        else:
+            return render_template('Incorrect.html',
+                                   answer = answer,
+                                   submittedAnswer = submittedAnswer);
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    if request.method == 'GET':
+        return render_template('CreateQuestion.html');
+    elif request.method == 'POST':
+        question = request.form['question'];
+        answer = request.form['answer'];
+        title = request.form['title'];
+
+        # Redis code to save question and answer
+        
+
+        return render_template('CreatedQuestion.html',
+                               question = question);
+    return;
